7036f99c79279bbe4de9af866e89b8315b86a471
jungkumseok <jungkumseok@gmail.com>
2018-07-31 18:02:19 -0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,30 @@
+## Dependencies
+
+* Node.js
+* NPM
+* Git
+
+## Getting Started
+
+1. First clone this repository:
+
+```
+~$ git clone https://github.com/jungkumseok/ubc-vsp18-server.git
+```
+Alternatively, download or copy the contents of this repository.
+
+2. Install the dependencies:
+
+```
+~$ cd ubc-vsp18-server
+~$ npm install
+```
+
+3. Start the web server
+
+```
+~$ npm start
+```
+
+4. Open a browser and navigate to `http://localhost:3000/`
+5. The `public` directory is where the client application is served from. Put your application code here along with the `index.html` file.
---
+++ index.js
@@ -0,0 +1,29 @@
+var path = require('path');
+var express = require('express');
+
+var PORT = 3000;
+var STATIC_URL = path.resolve(__dirname, './public/');
+var IMAGE_URLS = [1,2,3,4,5,6,7,8].map(function(n){ return 'http://ece.ubc.ca/~kumseok/src/vsp2018/images/image'+n+'.jpg' })
+
+var app = express();	// create an express app
+
+// print some info on incoming request
+app.use(function printRequest(req, res, next){
+	console.log('['+(new Date()).toLocaleString()+'] Received '+req.method+' at '+req.originalUrl);
+	next();
+});
+
+// return the image list
+app.get('/images', function(request, response){
+	response.json(IMAGE_URLS);
+});
+
+// serve static files at the root url
+app.use('/', express.static(STATIC_URL));
+
+// start listening on port
+app.listen(PORT, function(){
+	console.log('Web Service started');
+	console.log('    listening on port '+PORT);
+	console.log('    serving directory '+STATIC_URL);
+});
